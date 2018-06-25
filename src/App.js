d740c389b77a59685c0f2422d3b376502f163a6e
Wilkins Leong <wilkins.leong@rea-group.com>
2018-06-25 17:08:01 +1000

    Add a controlled text area to the page
    This synchronises the texarea with the components state.
    
    That means that we can use this state for future functions.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = { text: "foo" }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Word Counter YO</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <textarea onChange={ (e) => this.setState({ text: e.target.value }) } value={this.state.text}></textarea>
+        <div>Word count: some Number</div>
+        <div>Time to read: some sdfad</div>
       </div>
     );
   }
